3f939135e47d082163050ba5be7469bf1903c34f
Reza.K <rezakarimzadeh@Rezas-MacBook-Air.local>
2024-04-20 15:03:47 +0330

    add password config to yml file

--- src/api/validations/mobilevalidator.go
+++ src/api/validations/mobilevalidator.go
@@ -1,8 +1,7 @@
 package validation
 
 import (
-	"log"
-	"regexp"
+	"CarSaleAd-Web-Api/common"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -15,10 +14,7 @@ func IranianMobileNumberValidator(fld validator.FieldLevel) bool {
 	if !ok {
 		return false
 	}
-	res, err := regexp.MatchString(`^09(1[0-9]|2[0-9]|3[0-9]|9[0-9])[0-9]{7}$`, value)
-	if err != nil {
-		log.Print(err.Error())
-	}
-	return res
+
+	return common.IranianMobileNumberValidate(value)
 
 }
