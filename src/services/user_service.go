ffaa06fd5098120dce0c5af65eaaaf527a9f8d95
Reza.K <rezakarimzadeh@Rezas-MacBook-Air.local>
2024-05-06 13:53:58 +0330

    func ExistsByEmail/MobileNumber/Username & GetDefaultRole Implement

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -4,7 +4,9 @@ import (
 	"CarSaleAd-Web-Api/api/dto"
 	"CarSaleAd-Web-Api/common"
 	"CarSaleAd-Web-Api/config"
+	"CarSaleAd-Web-Api/constants"
 	"CarSaleAd-Web-Api/data/db"
+	"CarSaleAd-Web-Api/data/models"
 	"CarSaleAd-Web-Api/pkg/logging"
 
 	"gorm.io/gorm"
@@ -36,4 +38,52 @@ func (s *UserService) SendOtp(req *dto.GetOtpRequest) error {
 	}
 	return nil
 
+}
+
+func (s *UserService) existsByEmail(email string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("email = ? ", email).
+		Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+
+}
+
+func (s *UserService) existsByUsername(username string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("username = ?", username).
+		Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByMobileNumber(mobileNumber string) (bool, error) {
+	var exists bool
+	if err := s.database.
+		Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("mobile_number = ?", mobileNumber).
+		Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) getDefaultRole() (roleId int, err error) {
+	if err = s.database.Model(&models.Role{}).
+		Select("id").
+		Where("name = ?", constants.DefaultRoleName).
+		First(&roleId).Error; err != nil {
+		return 0, err
+	}
+	return roleId, nil
 }
