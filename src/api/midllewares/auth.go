5d8f24f4cbd623696a850a63ca1712a1b13702c2
Reza.K <rezakarimzadeh@Rezas-MacBook-Air.local>
2024-05-10 16:40:37 +0330

    User Authorization implement

--- src/api/midllewares/auth.go
+++ src/api/midllewares/auth.go
@@ -54,3 +54,32 @@ func Authentication(cfg *config.Config) gin.HandlerFunc {
 	}
 
 }
+
+func Authorization(validRoles []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.Keys) == 0 {
+			c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -300))
+			return
+		}
+
+		rolesVal := c.Keys[constants.RolesKey]
+		if rolesVal == nil {
+			c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -301))
+			return
+		}
+
+		roles := rolesVal.([]interface{})
+		val := map[string]int{}
+		for _, item := range roles {
+			val[item.(string)] = 0
+		}
+		for _, item := range validRoles {
+			if _, ok := val[item]; ok {
+				c.Next()
+				return
+			}
+		}
+		c.AbortWithStatusJSON(http.StatusForbidden, helper.GenerateBaseResponse(nil, false, -301))
+
+	}
+}
